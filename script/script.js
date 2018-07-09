84fa02fb7a1cebaceee5013f61a4b4598c5b3173
Aretyc <40947317+Aretyc@users.noreply.github.com>
2018-07-09 10:51:02 +0200

    07.09 10:50

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
 
+
+
+
+function  start_animation()
+{
     $("#about_title_h1").css({
         left: "-1360px",
     });
@@ -11,20 +15,70 @@ $(document).ready(function(){
     });
     $("#nav_ul").css({
        top: '-1000px',
-      // transform: "rotate(-5deg)",
     });
-
-    
- 
-
     $("#about_title_h1").animate({left: '0px'},1000);
     $("#about_title_h2").delay(500).animate({left: '0px'},1000);
     $("#about_title_p").delay(1000).animate({opacity: '1',},2000);
 
-    
     $("#nav_ul").delay(500).animate({  
-       "top" : '0px', 
+    "top" : '0px', 
     },1000,function(){$("#nav_ul").addClass("Animate");});
-    
+}
+
+$(document).ready(function(){
+
+$("#preloader").css({
+    display : "flex",
 
 });
+
+
+/*
+    
+// tab 
+
+    $('#nav_ul').each(function() {
+        const $a = $(this).find('a');
+
+        //po kliknięciu na link...
+        $a.on('click', function(e) {
+            //podstawiamy pod zmienną $this kliknięty link
+            const $this = $(this);
+
+            //pobieramy href klikniętego linka
+            const href = $this.attr('href');
+            //pobieramy treść na którą wskazuje link
+            const $target = $(href);
+
+            //jeżeli ta treść w ogóle istnieje...
+            if ($target.length) {
+                e.preventDefault(); //przerwij domyślną czynność jeżeli istnieje zawartość zakładki - inaczej niech dziala jak link
+
+                //usuwamy z sąsiednich linków klasę active
+                $this.siblings('a').removeClass('active');
+                //klikniętemu linkowi dajemy klasę active
+                $this.addClass('active');
+
+                //podobne działanie robimy dla treści tabów
+                $target.siblings('.tab-content').removeClass('active');
+                $target.addClass('active');
+            }
+
+        });
+    });
+    */
+});
+
+$(window).on('load',function(){
+    
+    $("#preloader").css({
+        display : "none",
+    });
+    start_animation();
+});
+/*
+$(window).load(function() {
+    
+  //  $("#preloader").fadeOut()
+});
+*/
